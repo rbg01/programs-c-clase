365f60602df0cad33097c97ae47d8dba31875db6
Roberto Betés <robertbg08@gmail.com>
2016-03-01 19:06:40 +0100

    examen_2EV

---
+++ programs_clase/primos.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+bool es_primo(int candidato){
+
+    ((candidato/candidato == 1)&&(candidato/1 == candidato))
+}
+
+int main(int argc, const char **argv){
+
+
+
+
+
+	return EXIT_SUCCESS;
+}
