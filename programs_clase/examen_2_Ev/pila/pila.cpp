cf1b7845ac4c6064e4e6cf8766562615b8e948f3
Roberto Betés <robertbg08@gmail.com>
2016-03-04 12:29:31 +0100

    _pila

---
+++ programs_clase/examen_2_Ev/pila/pila.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define N 15
+
+struct Pila {
+    int data[N];
+    int primera_pos_libre;
+};
+
+
+
+int main(int argc, const char **argv){
+
+
+
+
+
+	return EXIT_SUCCESS;
+}
