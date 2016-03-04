16561983f4286a8a9aa166a24dfa663bf68758d5
Roberto Betés <robertbg08@gmail.com>
2016-03-04 23:01:24 +0100

    _pila_mejorado

--- programs_clase/18_19_punteros/pila/pila.cpp
+++ programs_clase/18_19_punteros/pila/pila.cpp
@@ -1,25 +1,38 @@
-#include <stdio.h>
+#include<stdio.h>
 #include <stdlib.h>
-#include <stdio_ext.h>
+//#include <stdio_ext.h>
 
 #define N 0x10
 
-        /* Función sacar */
+        /* Función sacar: le metemos la pila, y un puntero a un entero
+         * que será la primero_posicion_libre */
 int sacar(int datos[N], int *cima) {
 
-    (*cima)--; // con ésto movemos el puntero al aray hacia atrás
+
+      /*añadimos detección para no sacar más números de la cuenta.
+       * si el elemento al que apunta cima es el último elemento
+       * de la pila,,no podemos sacar más*/
+    if (*cima == datos[0]){
+        printf("No hay más números que sacar!!\nSólo meter!!\n");
+        return 0;
+    }else
+        (*cima)--; // con ésto movemos el puntero al aray hacia atrás
     return datos[*cima];
 }
 
-        /* Función meter */
+        /* Función meter: le mandamos el numero a introducir,
+         * la plila, y un puntero a la primera_posicion_libre */
 void meter(int nuevo, int pila[N], int *cima){
-    pila[*cima] = nuevo; /*metemos el nuevo dato en la posición ded dnd
+    pila[*cima] = nuevo; /*metemos el nuevo dato en la posición dd dnd
                            sacamos el anterior*/
-    (*cima)++; //...y movemos el puntero hacia la derecha
+    (*cima)++; //...y movemos el puntero hacia arriba,
+   // señalando a la siguiente posición libre
 }
 
 
-        /* Función paara ver lo que hay en la pila*/
+        /* Función paara ver lo que hay en la pila: le mandamos la pila,
+         * un entero que será la posicion libre, para imprimir hasta ahí,
+         * y un numero 'bandeja' que es el último numero q sacamos de la pila */
 void mostrar_estado(int pila[N], int cima, int bandeja) {
     for (int i=0; i<cima; i++)
         printf("Fila %i: %i\n", i, pila[i]);
@@ -34,11 +47,11 @@ int main(int argc, const char **argv){
     int primera_pos_libre = 0;
     int entrada, salida;
 
-    while(1){
-        entrada = -1;
+    do{
+
         printf(": ");
         scanf(" %i", &entrada);
-        __fpurge(stdin);
+        fpurge(stdin);
         printf("entrada = %i\n", entrada);
 
         if (entrada < 0)
@@ -47,11 +60,15 @@ int main(int argc, const char **argv){
             meter(entrada, pila, &primera_pos_libre);
 
         mostrar_estado(pila, primera_pos_libre, salida);
-    }
-}
+    }while(1);
+
 
 
 
 
 return EXIT_SUCCESS;
 }
+
+
+
+
