da11543b16007c71b8d4deafbc49bba0e9146f92
Roberto Betés <robertbg08@gmail.com>
2016-03-06 05:05:57 +0100

    _genera_perfectos

--- programs_clase/examen_2_Ev/perfectos.cpp
+++ programs_clase/examen_2_Ev/perfectos.cpp
@@ -1,15 +1,45 @@
+/* Calcular numeros perfectos */
+
 #include <stdio.h>
 #include <stdlib.h>
 
 #define N 5
 
+bool es_perfecto(int candidato){
+
+    int sum_div=0;
+
+    for (int posible=1; posible<=candidato/2; posible++)
+        if (candidato % posible == 0)
+            sum_div += posible;
+    //si el candidato == 1 y candidato = suma de los divisores, 
+    //tenemos un númer perfecto
+    return candidato == 1 || candidato == sum_div;
+
+}
+
 int main(int argc, const char **argv){
 
 
+    int encontrados = 0;
+    int este_numero = 1;
+
+    int perfecto[N];
+
+    while (encontrados < N)
+        if (es_perfecto(este_numero++))
+            perfecto[encontrados++] = este_numero - 1;
+
+    for (int i=0; i<N; i++)
+        printf(" %i", perfecto[i]);
+
+    printf("\n");
 
 
 
 
 
-	return EXIT_SUCCESS;
+
+
+    return EXIT_SUCCESS;
 }
