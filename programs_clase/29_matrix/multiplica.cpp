96317f709c10912ce5cbebe4157a5f771df09b74
Roberto Betés <robertbg08@gmail.com>
2016-03-08 23:00:37 +0100

    _Fórmula_redirecc

--- programs_clase/29_matrix/multiplica.cpp
+++ programs_clase/29_matrix/multiplica.cpp
@@ -1,28 +1,23 @@
 #include "multiplica.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 void multiplica(int *matriz_a, int *matriz_b, int *matriz_c, int fils, int cols){ 
 
     int elemento=0;
     
 
-    for ( int f=0; f<fils; f++ ){ //-->hay q multiplicar cada elem de a por el correspondiente
-                                  //de b
+    for ( int f=0; f<fils; f++ ){ //-->hay q multiplicar cada elem de a por el correspondiente de b
 
-        for ( int c=0; c<cols; c++ ){
+        for ( int c=0; c<cols; c++ ){}
 
-          elemento += *(matriz_a + f*cols + c) * (matriz_b + f*cols +c);
-
-
-        }
-        
-            *(matriz_c + f*cols +c) = elemento
-    }
+          elemento += *(matriz_a+(f*ancho_A+c)) * *(matriz_b+(f*ancho_B +c);
+                  }
 
 
+                  c=0;
 
 
-
-
+                  }
 }
 
