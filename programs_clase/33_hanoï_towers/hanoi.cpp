6059b0462d55573482e0853982a6dac7247aa6d1
Roberto Betés <robertbg08@gmail.com>
2016-03-10 19:36:43 +0100

    _falla

--- programs_clase/33_hanoï_towers/hanoi.cpp
+++ programs_clase/33_hanoï_towers/hanoi.cpp
@@ -8,84 +8,112 @@
 #include <strings.h>
 
 #define MAX_DISC 10
+#define MAX_PILA 10
 #define PALOS 3
 
 
 
-    //Estructura Palo
-    struct Palo{
+//Estructura Palo
+struct Palo{
 
-        int cima;
-        int discos[MAX_DISC];
-    };
-
-
-    //Función inicializar la estructura todo a cero, menos el palo 0
-    void init(struct Palo *palo){
+    int cima;
+    int discos[MAX_DISC];
+};
 
 
-        if (palo[0]){//-->condición para inicializar  palo[0] con todos pos discos en la pila
-            palo-->cima = MAX_DISC; //--> ...y la cima del palo[0] con los 10 discos
-            for(int i=1; i<=MAX_DISC; i++)
-                palo-->discos[i];   //--> no?
+//Función inicializar la estructura todo a cero, menos el palo 0
+void init(struct Palo *palo){
 
+    bzero(palo, sizeof(struct Palo)); //-->Ponemos a cero toda la estructura
+}
+
+
+//Función push
+bool push(struct Palo *palo, int dato){
+
+    //-->comprobación del dato q hacemos push
+    if (dato > MAX_PILA) //-->si el tamaño de discos > que el MAXIMO
+        return false;       //-->...adiós...
+
+    if (palo->cima >= MAX_PILA) //-->si la cima >= q el MAXIMO, están todos los discos
+        return false; //-->...adiós..
+    //-->si cima > 0 y el dato > que el que hay puesto
+    if (palo->cima > 0 && dato > palo->discos[palo->cima-1])
+        return false; //-->..adiós..
+
+
+    palo->discos[palo->cima++] = dato;
+
+    return true;    //-->si no, todo bien y cima++
+}
+
+//Función pull
+int pull(struct Palo* palo){
+
+    return palo->discos[--palo->cima];  //-->devuelve el disco q hay y --cima
+}
+
+
+//Función mover disco
+void pintar(int cuanto, int dibujo){
+    for (int i=0; i<cuanto; i++)
+        printf("%c", dibujo);
+
+
+}
+
+
+
+int main(int argc, const char **argv){
+
+
+    int destino=0;  //-->Palo de destino
+    int radio=0;    //-->Diametro de los discos
+    struct Palo palo[PALOS];  //-->palo[PALOS] es una variable de Tipo de dato 'struct Palo'
+    int origen = 0;
+
+    //Inicializar palo[PALOS] a cero
+
+    for (int p=0; p<PALOS; p++)
+        init(&palo[p]);
+
+    for (int disco=5; disco>0; disco--)
+        push(&palo[0], disco);
+
+
+    while(1){
+
+        system("clear");
+
+        for (int fila=MAX_PILA-1; fila>=0; fila--) {
+            for (int n_palo=0; n_palo<PALOS; n_palo++) {
+                printf("\t");
+                int asteriscos = palo[n_palo].discos[fila];
+                int espacios   = MAX_DISC - asteriscos;
+                if (fila >= palo[n_palo].cima){
+                    espacios = MAX_DISC;
+                    asteriscos = 0;
+                }
+                pintar( espacios, ' ' );
+                pintar( asteriscos, '*' );
+                printf("|");
+                pintar( asteriscos, '*' );
+                pintar( espacios, ' ' );
+            }
+            printf("\n");
         }
-        bzero(palo, sizeof(struct Palo)); //-->Ponemos a cero toda la estructura
+
+        do {
+            printf("\norigen, destino: ");
+            scanf(" %i, %i", &origen, &destino);
+        } while(destino > PALOS || destino < 0 );
+        push( &palo[--destino], pull(&palo[--origen]) );
     }
 
 
-    //Función push
-    bool push(struct Palo *palo, int dato){
-
-        if ((palo->cima >= MAX_DISC)||(palo->discos < dato)) //-->si cima > 10 o el disco que
-            // hay en discos es < q el disco que metemos....
-            return false;       //-->...te peinas...
-
-        palo->discos[palo->cima++] = dato;
-
-        return true;    //-->si no, todo bien
-    }
-
-    //Función pull
-    int pull(struct Palo* palo){
-
-        return palo->discos[--palo->cima];
-    }
-
-    //Función mover disco
-
-
-
-    int main(int argc, const char **argv){
-
-
-        int destino=0;  //-->Palo de destino
-        int radio=0;    //-->Diametro de los discos
-        struct Palo palo[PALOS];  //-->palo[PALOS] es una variable de Tipo de dato 'struct Palo'
-
-        //Inicializar palo[PALOS] a cero
-
-        for (int p=0; p<PALOS)
-            init(&palo[p]);
-
-
-        while (1){
-        printf("Mover del palo: \nal disco: \n");
-        scanf(" %i, %i", &destino, &radio);
-
-        muevelo(palo[destino], radio);
-
-        if(muevelo)
-            continue;
-        else
-            printf ("error: ");
-
-        }
 
 
 
 
-
-
-        return EXIT_SUCCESS;
-    }
+return EXIT_SUCCESS;
+}
