174a379d7812bf958c1d3a3be445a5bf93f824af
Roberto Betés <robertbg08@gmail.com>
2016-03-13 23:30:09 +0100

    _fgets

--- programs_clase/05_scanf/buffer.cpp
+++ programs_clase/05_scanf/buffer.cpp
@@ -1,3 +1,6 @@
+
+  /* fgets */
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -7,7 +10,7 @@ int main(){
 
     printf("say something: ");
     //scanf(" %s", buffer);
-    fgets(buffer, 6, stdin);
+    fgets(buffer, 6, stdin);  //-->coge 6 char de stdin y ponmelos en buffer
     printf("%s\n", buffer);
 
 
