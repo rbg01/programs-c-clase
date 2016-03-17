599a4622835960b95897352c4e6b00750f7ac013
Roberto Betés <robertbg08@gmail.com>
2016-03-17 17:53:13 +0100

    _fibo_con_funcion

--- programs_clase/15_repaso/fibonacci.cpp
+++ programs_clase/15_repaso/fibonacci.cpp
@@ -1,6 +1,6 @@
 
 
-/*sucesión de Fibonacci
+/*sucesión de Fibonacci con funcion calcular
  * a0=1,a1=1,a2=2,a3=3,a4=5,a5=8.....an=(an-1)+(an-2)*/
 
 #include <stdio.h>
@@ -10,8 +10,18 @@
 #define ANTERIOR 1
 #define DOSANTES 2
 
-  /* Función calcular numero*/
+  /* Función calcular numero, le mando el puntero, y hago los cálculos*/
 void calcular(int *datos){
+
+    for (int i=0; i<TOPE; i++){
+
+        if(i==0 || i==1)
+            datos[i]=1;
+        else
+            datos[i] = datos[i-ANTERIOR] + datos[i-DOSANTES];
+    }
+
+
 
 
 }
@@ -19,25 +29,11 @@ int main(int argc, const char **argv){
 
 
     int fibo_data[TOPE];
-    
-
-    //fibo_data=NULL;
-
-    for (int cont = 0,int aux=0; cont < TOPE, aux < 2; cont++, aux++){
 
 
-  
-        //reserva dinamica de memoria
-      //  fibo_data = (int *) realloc((fibo_data),(cont + 1) * sizeof(int));
+    calcular(fibo_data);
 
-        if(cont < DOSANTES){
-            *(fibo_data+aux) = 1;
-            
-        
-        }break;
-        *(fibo_data+cont) = *(fibo_data+cont)-ANTERIOR + *(fibo_data+cont)-DOSANTES;
-  
-    }
+
 
 
     for (int cont = 0; cont < TOPE; cont++)
