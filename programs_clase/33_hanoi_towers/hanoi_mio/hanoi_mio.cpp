b1b5de4cb6cef798eb817832dffdb3dfcbac92aa
Roberto Betés <robertbg08@gmail.com>
2016-03-18 12:04:04 +0100

    _falta

--- programs_clase/33_hanoi_towers/hanoi_mio/hanoi_mio.cpp
+++ programs_clase/33_hanoi_towers/hanoi_mio/hanoi_mio.cpp
@@ -10,6 +10,7 @@
 struct Pila {
 
     int cima;
+    int pos;
     int datos[MAX_PILA];
 };
 
@@ -20,4 +21,24 @@ void init (struct Pila *pila){
     bzero(pila, sizeof(struct Pila));
 }
 
+  // Función intro
+bool in_tro(struct Pila *pila, int dato){
 
+    if (dato > MAX_PILA)
+        return false;
+    if (pila->cima >= MAX_PILA)
+        return false;
+    if (dato > pila->datos[pila->cima-1])
+        return false;
+    else
+        pila->datos[pila->cima++] = dato;
+    pila->pos_libre++;
+    return true = dato;
+  
+}
+
+  // Función outro
+int out_tro(struct Pila *pila){
+
+    return
+}
