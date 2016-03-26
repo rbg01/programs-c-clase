2026ad50b378ae808dcb21d2bc3776bcb77a79a4
Roberto Betés <robertbg08@gmail.com>
2016-03-26 18:49:16 +0100

    _factorial_recursividad

--- programs_mios/factorial/factorial.cpp
+++ programs_mios/factorial/factorial.cpp
@@ -1,27 +1,29 @@
-/*calculo de factoriales de forma recirsiva*/
+/*calculo de factoriales de forma recursiva*/
 
 #include <stdio.h>
 #include <stdlib.h>
 
-long int factorial(int n); /*prototipo de función*/
+long int factorial(int n){ /*definición de función*/
 
-int main(){
-
-    int n;
-    /*leer cantidas entera*/
-    printf("n = ");
-    scanf ("%d", &n);
-    /*calcualar y visualizar el factorial*/
-    printf("n! = %ld\n", factorial(n));
-
-	return EXIT_SUCCESS;
-}
-
-long int fanctorial(int n){
 
     if (n <= 1)
 	return (1);
     else
 	return (n * factorial(n -1));
 
+
+
 }
+
+int main(int argc, const char **argv){
+
+    int numero;
+    /*leer cantidas entera*/
+    printf("Introduce un número:  ");
+    scanf ("%d", &numero);
+    /*calcualar y visualizar el factorial*/
+    printf("%i! = %ld\n",numero, factorial(numero));
+
+	return EXIT_SUCCESS;
+}
+
