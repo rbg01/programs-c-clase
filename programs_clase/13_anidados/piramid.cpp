027237270179301bd63945bb183998eacf872f50
Roberto Betés <robertbg08@gmail.com>
2016-03-26 14:48:27 +0100

    _pirámide

--- programs_clase/13_anidados/piramid.cpp
+++ programs_clase/13_anidados/piramid.cpp
@@ -1,30 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define GROSOR 1
 
-void dibuja(char dibujo){
 
-    printf("%c ", dibujo);
+void dibuja(char dibujo, int veces){
+
+    for(int i=0; i<veces; i++)
+    printf("%c", dibujo);
 }
 
 int main(int argc, const char **argv){
 
-    int lado,
-	fila,
-	col;
-
-    printf("dame un lado ");
-    scanf(" %i", &lado);
-      
-    for (fila=0; fila<lado; fila++){
-      
-            for (col=0; col<fila; col++)
-            dibuja('*');
-            printf("\n");
-
-            
-
+    int altura,
+        base,
+	h;
 	
+    int veces_espacios,
+        veces_asterisc;
+
+    printf("dame una altura: \n");
+    scanf(" %i", &altura);
+    base = altura * 2;
+
+    veces_espacios = altura - GROSOR;
+    veces_asterisc = GROSOR * 2;
+
+    for (h=0; h<altura; h++){
+
+        dibuja(' ', veces_espacios);
+        dibuja('*', veces_asterisc);
+        dibuja(' ', veces_espacios);
+
+        veces_espacios -= 1;
+        veces_asterisc += 2;
+ 
+            printf("\n");
     }
 
 
