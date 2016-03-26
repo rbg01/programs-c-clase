a40742a2b4e327a83357333b141c4475b74fae09
Roberto Betés <robertbg08@gmail.com>
2016-03-26 13:23:07 +0100

    _marco_variable_con_relleno

--- programs_clase/13_anidados/cuadrado_2.cpp
+++ programs_clase/13_anidados/cuadrado_2.cpp
@@ -1,30 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define GROSOR 1
+
+
+void imprime(char dibujo){
+
+    printf("%c ", dibujo);
+}
+
+
 int main(int argc, const char **argv){
 
-    
-    int entrada;
 
-    printf("dame un lado ");
-    scanf(" %i", &entrada);
-        
-    for (int fila=0; fila < entrada; fila++){
-        
-            for (int col=0; col < entrada; col++)
+  
+    int ancho;
+    int alto; 
 
-                if ( col<fila )
-                    printf("O ");
+    printf("Dime una altura: ");
+    scanf(" %i", &alto);
+    printf("Dime una anchura: ");
+    scanf(" %i", &ancho);
 
-                else
-                    printf("* ");
-            printf("\n");
+    for (int fila=0; fila < alto; fila++){
+
+        for (int col=0; col < ancho; col++){
+
+            if((fila > 0 && fila < alto-GROSOR) && (col > 0 && col < ancho-GROSOR))
+                imprime('O');
+            else
+                imprime('*');
+
+
+
+        }
+
+        printf("\n");
     }
 
 
 
-
-
-
-	return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
