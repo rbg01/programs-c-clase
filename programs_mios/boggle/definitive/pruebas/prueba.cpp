2e5dd0d6677f12424c0b7018aad61a3c22dae450
Roberto B <robertbg08@gmail.com>
2016-04-01 00:55:23 +0200

    _probando

--- programs_mios/boggle/definitive/pruebas/prueba.cpp
+++ programs_mios/boggle/definitive/pruebas/prueba.cpp
@@ -25,93 +25,122 @@ const char dados[16][6] = {
 };
 
 
-void busca_palabra(char *tablero, bool *prueba, char *palabra){
+void busca_palabra(char *tabl, bool *prueba, char *palabra){
 
     int fin = strlen(palabra)-1;
     int match = 0;
-    printf("%i", fin);
+
+    printf("%i\n", fin);
 
 
-    return;
-}
+    for( int i=0; i<fin; i++){
+        char letra = *(palabra+i);
+        printf("%c ", letra); 
+        for (int f=0; f<4; f++){
+            for(int c=0; c<4; c++){
+                if (*(tabl+f*4+c)==letra && *(prueba+f*4+c)== false){
+                    *(prueba +f*4 +c) = true;
+                    match += 1;
+                    //break;
+                }else if (*(tabl+f*4+c)==letra && *(prueba+f*4+c)== true)
 
-void tira_dados(const char *dados,char *letras){
+                        match -= 1;
 
-
-    int dad=0;
-
-    for (int fil=0; fil<4; fil++){
-        for (int col=0; col<4; col++, dad++){
-            *(letras + fil*4 + col) = *(dados + dad + (rand()%6));
+            }
         }
     }
-    return ;
 
-}
-
-void ponga_false(bool *tablero){
-
-    for(int f=0; f<4; f++){
-        for(int c=0; c<4; c++)
-            *(tablero+f*4+c)= false;
-    }
-    return ;
-
-}
-
-int main(int argc, const char **argv){
-
-
-    srand (time(NULL));
-
-    char letras[4][4],
-         tablero[4] [4],
-         palabra[25];
-    bool busqueda[4][4];
-
-
-    ponga_false(*busqueda);
-
-
-    tira_dados(*dados, *tablero);
-
-    for (int f=0; f<4; f++){
-        for (int c=0; c<4; c++)
-            printf("%c ", tablero[f][c]);
         printf("\n");
+        if (match==fin)
+            printf("palabra encontrada\n");
+        else printf("Palabra NO encontrada\n");
+        return;
     }
 
-    printf("\n");
+    void tira_dados(const char *dados,char *letras){
 
-    for (int f=0; f<4; f++){
-        for (int c=0; c<4; c++)
-            printf("%i ", busqueda[f][c]);
+
+        int dad=0;
+
+        for (int fil=0; fil<4; fil++){
+            for (int col=0; col<4; col++, dad++){
+                *(letras + fil*4 + col) = *(dados + dad + (rand()%6));
+            }
+        }
+        return ;
+
+    }
+
+    void ponga_false(bool *tablero){
+
+        for(int f=0; f<4; f++){
+            for(int c=0; c<4; c++)
+                *(tablero+f*4+c)= false;
+        }
+        return ;
+
+    }
+
+    int main(int argc, const char **argv){
+
+
+        srand (time(NULL));
+
+        char letras[4][4],
+             tablero[4] [4],
+             palabra[25];
+        bool busqueda[4][4];
+
+
+        ponga_false(*busqueda);
+
+
+        tira_dados(*dados, *tablero);
+
+        for (int f=0; f<4; f++){
+            for (int c=0; c<4; c++)
+                printf("%c ", tablero[f][c]);
+            printf("\n");
+        }
+
         printf("\n");
+
+        for (int f=0; f<4; f++){
+            for (int c=0; c<4; c++)
+                printf("%i ", busqueda[f][c]);
+            printf("\n");
+        }
+
+        printf("\n");
+
+        printf("introduce palabra: \n");
+        fgets(palabra,25,stdin);
+
+        //Pongo palabra en MAYS.
+        for(int i=0; palabra[i]!='\0'; i++){
+            printf("%c ", palabra[i]);
+            palabra[i]=toupper(palabra[i]);
+        }
+
+        for(int i=0; palabra[i]!='\0'; i++){
+            printf("%c ", palabra[i]);
+        }
+
+        busca_palabra(*tablero, *busqueda, palabra);
+
+
+        for (int f=0; f<4; f++){
+            for (int c=0; c<4; c++)
+                printf("%i ", busqueda[f][c]);
+            printf("\n");
+        }
+
+
+
+
+
+
+
+
+        return EXIT_SUCCESS;
     }
-
-    printf("\n");
-
-    printf("introduce palabra: \n");
-    fgets(palabra,25,stdin);
-
-    //Pongo palabra en MAYS.
-    for(int i=0; palabra[i]!='\0'; i++){
-        printf("%c ", palabra[i]);
-                palabra[i]=toupper(palabra[i]);
-    }
-
-    for(int i=0; palabra[i]!='\0'; i++){
-        printf("%c ", palabra[i]);
-     }
-
-    busca_palabra(*tablero, *busqueda, palabra);
-
-
-                
-
-
-
-
-
-    return EXIT_SUCCESS;
-}
