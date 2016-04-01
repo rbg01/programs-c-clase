a1047477206145b59a25512bbbfcc93822007ac8
Roberto B <robertbg08@gmail.com>
2016-04-01 21:30:32 +0200

    _buscar_palabra

--- programs_clase/36_search/buscar_palabra.cpp
+++ programs_clase/36_search/buscar_palabra.cpp
@@ -1,50 +1,64 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+#include <string.h>
 
+#define LADO 16
+#define MAXLETRAS 27
+
+void rellenar_tablero(const char *origen, char *destino){
+
+    for(int f=0; f<LADO; f++){
+        for(int c=0; c<LADO; c++)
+            *(destino + f*LADO +c) = *(origen + rand()%MAXLETRAS);
+
+    }
+    return;
+}
+
+void pintar_tablero(char *tablero){
+
+    for(int f=0; f<LADO; f++){
+        printf("\t");
+        for(int c=0; c<LADO; c++)
+            printf("%c%i ",*(tablero + f*LADO +c),rand()%MAXLETRAS);
+        printf("\n");
+
+    }
+
+
+}
 int main(int argc, const char **argv){
 
-    char tablero[] = {'x','w','p','a','j','b','p','a','c','h','p','a','c','o','a','r','\0'};
-    char palabra[] = {'p','a','c','o','\0'};
-    int encontrar = 0;
-    int buscar = 0;
-    int fin = 4;
-    int match = 0;
+    srand(time(NULL));
 
-/*
-    for (int i=0; tablero[i] != '\0'; i++){
-        printf("%c , %c", tablero[i], palabra[i]);
+    const char abcedario[] = {"ABCDEFGHIJKLMNOPQRSTUVWXYZ"};
+    char tablero[LADO][LADO];
+    int prueb[LADO][LADO];
 
-    }
-    */
+    for(int i=0; i<(int)strlen(abcedario); i++)
+        printf("%c", abcedario[i]);
+    printf("%i", (int)strlen(abcedario));
+    printf("co%co", 165);
+
+    rellenar_tablero(abcedario, *tablero);
+
+    pintar_tablero(*tablero);
 
 
-    for(buscar, encontrar; buscar<fin, encontrar != '\0'; buscar++, encontrar++ ){
-        if(tablero[encontrar] == palabra[buscar]){
-            encontrar += 1;
-            buscar += 1;
-            match += 1;
-        
-        else if(buscar != 0){
-            encontrar == encontrar;
-            buscar = 0;
-        
-        }else
-            encontrar += 1;
-            buscar = 0;
-        
-        
-        
-        printf("NO");
-        }
+    for(int f=0; f<LADO; f++){
+        printf("\t");
+        for(int c=0; c<LADO; c++){
+            prueb[f][c] = rand()%MAXLETRAS;
+            printf("%i ", prueb[f][c]);
+                    }
+        printf("\n");
+
     }
 
 
-    if(match == fin)
-    printf("Palabra NO encontrada.%i\n", match);
-    else printf("Palabra encontrada.\n");
-    printf("%i %i", buscar, fin);
+    
 
 
-
-	return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
