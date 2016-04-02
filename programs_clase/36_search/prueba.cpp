3d46147d56d0a26333d449904d04d3d7a5ecde2f
Roberto B <robertbg08@gmail.com>
2016-04-02 14:27:56 +0200

    _pruebas

---
+++ programs_clase/36_search/prueba.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+
+
+
+int main(int argc, const char **argv){
+
+
+    char palabra_1[25],
+         palabra_2[25];
+
+
+    printf("\tIntroduce una palabra a buascar:\n");
+    fgets(palabra, 25, stdin);
+    printf("%i", (int)strlen(palabra));
+
+    printf("introduce una palabra:\n");
+    for(int i=0; getchar)
+
+
