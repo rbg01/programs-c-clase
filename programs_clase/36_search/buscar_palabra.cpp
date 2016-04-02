7159c0ddf60ef3064b071451e2c3aa653977d21e
Roberto B <robertbg08@gmail.com>
2016-04-02 14:27:01 +0200

    _buscar_palabra

--- programs_clase/36_search/buscar_palabra.cpp
+++ programs_clase/36_search/buscar_palabra.cpp
@@ -2,9 +2,11 @@
 #include <stdlib.h>
 #include <time.h>
 #include <string.h>
+#include <ctype.h>
 
 #define LADO 16
-#define MAXLETRAS 27
+#define MAXLETRAS 31
+#define MAXPALABRA 35
 
 void rellenar_tablero(const char *origen, char *destino){
 
@@ -21,42 +23,64 @@ void pintar_tablero(char *tablero){
     for(int f=0; f<LADO; f++){
         printf("\t");
         for(int c=0; c<LADO; c++)
-            printf("%c%i ",*(tablero + f*LADO +c),rand()%MAXLETRAS);
+            printf("%c ",*(tablero + f*LADO +c));
         printf("\n");
 
     }
+  
+    return;
 
+}
 
+bool buscar_letra(char *tablero, char *palabra){
+
+    for(int f=0; f<LADO; f++){
+        for(int c=0; c<LADO; c++)
+            if(*(tablero+f*LADO+c)!= *palabra)
+                return false;
+    }
+    return true;
+}
+
+void pon_mays(char palabra[], int fin){
+
+    for(int i=0; i<fin; i++)
+         toupper(palabra[i]);
+    
+    return;
 }
 int main(int argc, const char **argv){
 
     srand(time(NULL));
 
-    const char abcedario[] = {"ABCDEFGHIJKLMNOPQRSTUVWXYZ"};
-    char tablero[LADO][LADO];
+    const char abcedario[] = {"ABCADEUFGHIJKELMNOPQIRSTUVWXOYZ"};
+    char tablero[LADO][LADO],
+         palabra[MAXPALABRA];
     int prueb[LADO][LADO];
+    bool letra_encontrada;
 
-    for(int i=0; i<(int)strlen(abcedario); i++)
-        printf("%c", abcedario[i]);
-    printf("%i", (int)strlen(abcedario));
-    printf("co%co", 165);
+    system("clear");
 
     rellenar_tablero(abcedario, *tablero);
 
     pintar_tablero(*tablero);
 
+    printf("\n");
 
-    for(int f=0; f<LADO; f++){
-        printf("\t");
-        for(int c=0; c<LADO; c++){
-            prueb[f][c] = rand()%MAXLETRAS;
-            printf("%i ", prueb[f][c]);
-                    }
-        printf("\n");
+    printf("\tIntroduce una palabra a buascar:\n");
+    fgets(palabra, MAXPALABRA, stdin);
 
-    }
+    int fin_palabra = ((int)strlen(palabra)-1);
 
+    pon_mays(palabra, fin_palabra); 
 
+    letra_encontrada = buscar_letra(*tablero, palabra);
+
+    if(letra_encontrada!=true)
+        printf("letra No encontrada");
+    else
+        printf("letra encontrada");
+   
     
 
 
