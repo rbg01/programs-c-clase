8bb4617cf5715551e467ecc58d9ba99ab9440139
Roberto B <robertbg08@gmail.com>
2016-04-10 15:43:39 +0200

    _piramide_de_O

--- programs_clase/c_statements/04_triangles/04_triangle.cpp
+++ programs_clase/c_statements/04_triangles/04_triangle.cpp
@@ -4,27 +4,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+void pintar(char dibujo, int veces){
+
+    for (int i=0; i<veces; i++)
+        printf("%c", dibujo);
+}
+
 int main(int argc, const char **argv){
 
-    
-    int entrada = 4;
 
-    
-    for (int fila=0; fila < entrada+1; fila++){
-        
-            for (int col=0; col < entrada; col++)
+    int altura,
+        base,
+        veces_espacios = 0;
+    int veces_caracter = 0;
 
-                if ( col<fila )
+    printf("Dime una altura: \n");
+    scanf(" %i", &altura);
 
-                    printf("O ");
+    base = altura + (altura - 1);
+    veces_espacios = altura - 1;
+    veces_caracter = altura - (altura-1);
 
-                else
-                    printf(" ");
-            printf("\n");
+    for (int h=0; h<altura; h++){
+
+        pintar(' ', veces_espacios);
+        pintar('O', veces_caracter);
+
+        veces_espacios -= 1;
+        veces_caracter += 2;
+
+        printf("\n");
+
+
     }
-
-
-
 
 
 
