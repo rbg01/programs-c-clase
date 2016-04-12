cf6c4872d296d4d958edf19f121b8f225de15202
Roberto B <robertbg08@gmail.com>
2016-04-12 11:04:34 +0200

    _argc_y_argv

--- programs_clase/c_statements/01_simple_output/repeat.cpp
+++ programs_clase/c_statements/01_simple_output/repeat.cpp
@@ -1,12 +1,14 @@
+
+
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, const char **argv){
+int main(int argc, char **argv){
 
-    char text[] = argv[1];
+    char *text = argv[1];
 
     for (int i=0; i<10; i++)
-	printf ("%s", text);
+	printf ("%s\n", text);
 
 
 
