1594da8a8338b29c3c54c6842d1d4d523ca2ca5e
Roberto B <robertbg08@gmail.com>
2016-04-23 17:36:14 +0200

    _factorial_recursivo

---
+++ programs_mios/factorial/factorial_revisited/fact_recursive.cpp
@@ -0,0 +1,32 @@
+
+/*  Factorial versión recursiva */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int factorial(int n){
+
+
+    if(n >= 1){
+
+        return (n * factorial(n-1));
+    }
+    else
+
+    return (1);
+}
+
+
+int main(int argc, const char **argv){
+
+    int numero;
+
+    scanf(" %i", &numero);
+
+    printf("%i\n", factorial(numero));
+
+
+    return EXIT_SUCCESS;
+}
+
+
