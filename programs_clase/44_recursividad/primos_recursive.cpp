9bedfec3ac13534670b7c947d6e2fe2a32e64f38
Roberto B <robertbg08@gmail.com>
2016-04-28 09:35:18 +0200

    _primos_version_Tx

---
+++ programs_clase/44_recursividad/primos_recursive.cpp
@@ -0,0 +1,39 @@
+
+
+
+    /*  Primos Recursivos */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+bool es_primo (int candidato, int posible){
+
+    bool respuesta;
+
+    posible -= 1;
+
+    if (posible == 1)
+        return false;
+
+    respuesta = candidato % posible == 0;
+
+    return respuesta || es_primo(candidato, posible);
+
+}
+
+int main (int argc, const char **argv){
+
+
+    int numero;
+
+    scanf(" %i", &numero);
+
+    if (es_primo(numero, numero ))
+        printf("El número %i, NO primo.\n", numero);
+    else
+        printf("El número %i, es primo.\n", numero);
+
+
+    return EXIT_SUCCESS;
+}
