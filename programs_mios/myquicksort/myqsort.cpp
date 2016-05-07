c9ca84167487d67aa177138d5bb039fec89a234b
Roberto B <robertbg08@gmail.com>
2016-05-07 21:52:38 +0200

    _ordenando_Pff

--- programs_mios/myquicksort/myqsort.cpp
+++ programs_mios/myquicksort/myqsort.cpp
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MAX 15
+#define MAX 14
 
 void intercambia(int datos[MAX], int pos1, int pos2){
 
@@ -28,28 +28,31 @@ void max_num(int datos[MAX], int fin){
 
     int izq = fin - fin;
     int drch = izq + 1;
-    int limit;
+    static  int limit;
 
-    limit = fin-1;
+    limit = fin - 1;
 
-    for(int i=0; i < limit; i++){
-    while(drch < limit){
+    do{
+
+        //printf("izq=%i, drch=%i, limit=%i, fin=%i\n", izq, drch, limit, fin); 
 
         if(datos[izq] <= datos[drch]){
             izq++;
             drch++;
-        
-        }
-    
-            intercambia(datos, izq, drch);
-            imprime(datos);
-
-          // max_num(datos, limit);
 
         }
-    }
 
-   printf("izq=%i, drch=%i\n", izq, drch); 
+        intercambia(datos, izq, drch);
+        imprime(datos);
+
+    }while(drch < limit);
+
+    if(limit ==izq+1)
+        max_num(datos, fin-1);
+
+
+
+    printf("izq=%i, drch=%i, limit=%i, fin=%i\n", izq, drch, limit, fin); 
     return;
 
 }
@@ -57,18 +60,18 @@ void max_num(int datos[MAX], int fin){
 int main(int argc, const char **argv){
 
 
-    int datos[MAX] = {4, 5, 8, 6, 10, 3, 2, 7, 1, 15, 12, 9, 1, 10, 25};
+    int datos[] = {4, 5, 8, 6, 10, 3, 13, 7, 15, 12, 9, 1, 10, 2 };
 
     imprime(datos);
 
- //  for(int i=0; i <= MAX; i++)
-        max_num(datos, MAX);
-   
+    max_num(datos, MAX);
+
+
     imprime(datos);
 
 
 
 
 
-	return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
