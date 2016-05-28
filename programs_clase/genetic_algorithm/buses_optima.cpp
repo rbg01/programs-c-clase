9af385b0c67a3f04597d0b4b4df78a3cf441f7d9
Roberto B <robertbg08@gmail.com>
2016-05-28 20:09:45 +0200

    _revisar asignaciones al array penaltys

--- programs_clase/genetic_algorithm/buses_optima.cpp
+++ programs_clase/genetic_algorithm/buses_optima.cpp
@@ -7,6 +7,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <strings.h>
+
 
 #define MAX 10
 #define TOT_BUS 4
@@ -22,29 +24,45 @@ void haz_pattern(int array[][MAX]){
 
 }
 
-void imprime(int array[][MAX]){
+void imprime(int array[][MAX], int brray[]){
 
     for(int fil=0; fil<FILS; fil++){
         for(int col=0; col<MAX; col++){
             printf("%i ", array[fil][col]);
         }
-        printf("\n");
+        printf("-->penalty = %i\n", brray[fil]);
     }
 
 }
 
-void calcula_penalty(int array[][MAX], int penal[], int buses[]){
+void calcula_penalty(int array[MAX], int penal[], int buses[], int clase[], int orden){
+
+
 
     for(int bus=0; bus<TOT_BUS; bus++){
-        for(int fil=0; fil<FILS; fil++){
-            for(int col =0; col<MAX; col++){
+        int aux = 0;
+        for(int col=0; col<MAX; col++){
+            if(array[col] == buses[bus])
+                aux = aux + clase[col];
 
-                if(array[fil][col] == buses[bus])
-                    penalty += penalty + penal[col];
-            }
         }
+        int resultado;
+        resultado = MAX_BUS - aux;
+        printf("%i - %i = %i del bus %i, del pattern %i\n",MAX_BUS,aux,resultado,bus+1, orden);
+        if(resultado < MAX_BUS)
+            penal[orden] += resultado;
+
+        else
+            penal[orden] += resultado * -2;
+
     }
+    printf("%i\n", penal[orden]);
+
+
+
 }
+
+
 int main(int argc, const char **argv){
 
     srand(time(NULL));
@@ -52,12 +70,23 @@ int main(int argc, const char **argv){
     int clase_gente [] = {20, 15, 18, 32, 7, 10, 23, 13, 18, 12};
     int patterns[FILS][MAX];
     int autobuses[] = {1, 2, 3, 4};
-    int penaltys[MAX];
+    int penaltys[FILS];
+
+    bzero(penaltys,FILS);
 
     haz_pattern(patterns);
-    calcula_penalty(patterns, penaltys, autobuses);
-    imprime(patterns);
+    //Hacer un bucle para mandarle fila por fila a calcula_penalty
+    for(int i=0; i<FILS; i++)
+        calcula_penalty(patterns[i], penaltys, autobuses, clase_gente, i);
 
+
+
+
+
+
+
+
+    imprime(patterns, penaltys);
 
 
     return EXIT_SUCCESS;
