885030cc6a1b4527b9c42ba9c251ffd3c6c6687e
Roberto B <robertbg08@gmail.com>
2016-09-27 18:15:27 +0200

    uso de argc y argv

---
+++ programs_clase/16_varios/sumalo.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (int argc, const char **argv){
+
+    int op_1, op_2, resultado;
+
+    if (argc == 1)
+        printf("Ingresa dos numeros, si éso...\n");
+
+    for(int i=0; i < argc; i++)
+        printf ("%s\n", argv[i]);
+
+
+    op_1 = atoi(argv[1]);
+    op_2 = atoi(argv[2]);
+
+    printf("%s + %s = %i\n", argv[1], argv[2], op_1 + op_2);
+
+    return EXIT_SUCCESS;
+
+}
